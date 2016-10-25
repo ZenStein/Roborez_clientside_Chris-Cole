81c6f85e1c6888cfd2582c437939b766acd5a4e1
ZenStein <shaneenterprises@gmail.com>
2016-10-25 10:03:19 -0700

    init ag grid

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,12 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SmsMessagingComponent } from './sms-messaging/sms-messaging.component';
+import { MakeReservationComponent } from './make-reservation/make-reservation.component';
 import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
     {path:'sms_messaging',component: SmsMessagingComponent},
+    {path:'make_reservation',component: MakeReservationComponent },
     {path:'',component: HomeComponent}
 ];
 
--- src/app/make-reservation/make-reservation.component.ts
+++ src/app/make-reservation/make-reservation.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+//import { DatepickerModule } from 'angular2-material-datepicker'
+import {AgGridNg2} from 'ag-grid-ng2/main';
+import {GridOptions} from 'ag-grid/main';
 
 @Component({
   selector: 'app-make-reservation',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./make-reservation.component.css']
 })
 export class MakeReservationComponent implements OnInit {
+gridOptions:GridOptions;
+columnDefs
+today
 
-  constructor() { }
-
+  constructor() {
+      this.gridOptions = {
+          columnDefs: [
+              {headerName: 'Units', field: 'unit', width: 100, pinned: 'left'},
+              {headerName: '11/1', field: 'date', width: 100},
+              {headerName: '11/2', field: 'date', width: 100},
+              {headerName: '11/3', field: 'date', width: 100},
+              {headerName: '11/4', field: 'date', width: 100},
+              {headerName: '11/5', field: 'date', width: 100},
+              {headerName: '11/6', field: 'date', width: 100},
+              {headerName: '11/7', field: 'date', width: 100},
+              {headerName: '11/8', field: 'date', width: 100},
+              {headerName: '11/9', field: 'date', width: 100},
+              {headerName: '11/10', field: 'date', width: 100},
+              {headerName: '11/11', field: 'date', width: 100}
+          ],
+          rowData: [
+              {unit: '#1', role: 'Developer'},
+              {unit: '#2', role: 'Manager'},
+              {unit: '#3', role: 'Musician'},
+              {unit: '#4', role: 'Musician'},
+              {unit: '#5', role: 'Musician'},
+              {unit: '#6', role: 'Musician'},
+              {unit: '#7', role: 'Musician'},
+              {unit: '#8', role: 'Musician'},
+              {unit: '#9', role: 'Manager'}
+          ],
+          rowHeight: 48
+      };
+  }
+    
   ngOnInit() {
+
   }
 
 }
